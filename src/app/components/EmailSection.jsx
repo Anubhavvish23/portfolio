5076284c1698b73d9b2fd20701b854f65ffc543a
Anubhavvish23 <anubhavsanjay01@gmail.com>
2024-09-11 16:28:35 +0530

    EmailSection

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -1,6 +1,5 @@
 "use client";
 import React from "react";
-import Image from "next/image";
 import Link from "next/link";
 
 const EmailSection = () => {
@@ -13,27 +12,33 @@ const EmailSection = () => {
       <div className="z-10">
         <h5 className="text-xl font-bold text-white my-2">Let&apos;s Connect</h5>
         <p className="text-[#ADB7BE] mb-4 max-w-md">
-          I&apos;m currently looking for new opportunities, my inbox is always
-          open. Whether you have a question or just want to say hi, I&apos;ll
-          try my best to get back to you!
+        <p className="text-[#ADB7BE] mb-4 max-w-md">
+  I&apos;m currently looking for new opportunities, my inbox is always open.
+  Whether you have a question or just want to say hi, I&apos;ll try my best to
+  get back to you! You can also check out my company at{" "}
+  <a href="https://www.notarc.in" target="_blank" rel="noopener noreferrer" className="text-blue-500 underline">
+    www.notarc.in
+  </a>.
+</p>
+
         </p>
         <div className="socials flex flex-row gap-2">
           {/* Email link */}
           <Link href="mailto:anubhavsanjay01@gmail.com">
             <div className="icon-wrapper">
-              <Image src="/images/e.png" alt="Email Icon" width={50} height={50} />
+              <img src="https://upload.wikimedia.org/wikipedia/commons/4/4e/Gmail_Icon.png" alt="Email Icon" width={40} height={40} />
             </div>
           </Link>
           {/* LinkedIn link */}
           <Link href="https://www.linkedin.com/in/anubhav-s-14a380229?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app">
             <div className="icon-wrapper">
-              <Image src="/linkedin-icon.svg" alt="LinkedIn Icon" width={50} height={50} />
+              <img src="https://upload.wikimedia.org/wikipedia/commons/c/ca/LinkedIn_logo_initials.png" alt="LinkedIn Icon" width={40} height={40} />
             </div>
           </Link>
           {/* WhatsApp link */}
-          <Link href="https://wa.me/+919880502538"> {/* Replace 'yourphonenumber' with your actual number */}
+          <Link href="https://wa.me/+919880502538">
             <div className="icon-wrapper">
-              <Image src="/images/wp.png" alt="WhatsApp Icon" width={40} height={40} />
+              <img src="https://upload.wikimedia.org/wikipedia/commons/6/6b/WhatsApp.svg" alt="WhatsApp Icon" width={40} height={40} />
             </div>
           </Link>
         </div>
